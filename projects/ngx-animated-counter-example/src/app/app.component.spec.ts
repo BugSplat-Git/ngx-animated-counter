4f6ad89cbe03ead919e227951d4103f9dc3b13c0
Bobby Galli <bobbyg603@gmail.com>
2022-02-10 17:59:50 -0500

    chore: refactor test

--- projects/ngx-animated-counter-example/src/app/app.component.spec.ts
+++ projects/ngx-animated-counter-example/src/app/app.component.spec.ts
@@ -1,8 +1,11 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NgxAnimatedCounterModule } from '@bugsplat/ngx-animated-counter';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -14,21 +17,20 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.debugElement.componentInstance;
+  })
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
   it('should have set title to \'@bugsplat/ngx-animated-counter\'', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('@bugsplat/ngx-animated-counter');
+    expect(component.title).toEqual('@bugsplat/ngx-animated-counter');
   });
 
   it('should render 4 ngx-animated-counters', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelectorAll('ngx-animated-counter').length).toEqual(4);
   });
